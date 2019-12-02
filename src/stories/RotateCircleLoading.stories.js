58e34904865a809433654f9a6df2946a3c9adf67
xdSi <3450236968@qq.com>
2019-12-02 20:56:11 +0800

    docs: 给组件添加测试文档

--- src/stories/RotateCircleLoading.stories.js
+++ src/stories/RotateCircleLoading.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, number, text } from '@storybook/addon-knobs';
 import { RotateCircleLoading } from '~/components';
 import Container from './compoment/Container';
 storiesOf('RotateCircleLoading', module)
@@ -8,9 +8,13 @@ storiesOf('RotateCircleLoading', module)
   .add(
     'RotateCircleLoading',
     () => {
+      let speed = 1;
+      let color = '';
+      speed = number('动画速度(s)')
+      color = text('颜色')
       return (
         <Container>
-          <RotateCircleLoading></RotateCircleLoading>
+          <RotateCircleLoading speed={speed} color={color}></RotateCircleLoading>
         </Container>
       );
     }
--- src/stories/SemipolarLoading.stories.js
+++ src/stories/SemipolarLoading.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, number, text } from '@storybook/addon-knobs';
 import { SemipolarLoading } from '~/components';
 import Container from './compoment/Container';
 storiesOf('SemipolarLoading', module)
@@ -8,9 +8,13 @@ storiesOf('SemipolarLoading', module)
   .add(
     'SemipolarLoading',
     () => {
+      let speed = 1;
+      let color = '';
+      speed = number('动画速度(s)')
+      color = text('颜色')
       return (
         <Container>
-          <SemipolarLoading></SemipolarLoading>
+          <SemipolarLoading speed={speed} color={color}></SemipolarLoading>
         </Container>
       );
     }
